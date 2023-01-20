ab450c95986cd937f2f661b1c7fa01fd43dd7f2e
kavinrt <113418103+kavinrt@users.noreply.github.com>
2023-01-20 17:56:19 +0530

    Add files via upload

---
+++ Pattern/Numbersflow.java
@@ -0,0 +1,16 @@
+class Numbersflow
+{
+public static void main(String args[])
+{
+int n =5;
+
+for(int i=1;i<=n;i++)
+{
+for(int j=0;j<i;j++)
+{
+System.out.print(((i+j)%2)+"");
+}
+System.out.println();
+}
+}
+}
---
+++ Pattern/STriangle.java
@@ -0,0 +1,16 @@
+class STriangle
+{
+public static void main(String args[])
+{
+int n = 7;
+
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.print("@");
+}
+System.out.println();
+}
+}
+}
